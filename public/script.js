bb9c7cbf3a112f59fc6e1756d94d9f3e1b00a69a
achin4bacon <kendra.leigh.cooper@gmail.com>
2017-08-19 15:32:28 -0600

    organized microphone stuff; combined other work  part I about to test

--- public/script.js
+++ public/script.js
@@ -1,4 +1,43 @@
 $(document).ready(function() {
 	//Initialize WOW js
 	new WOW().init();
+
+	$('#signUp').click(function(){
+    	alert('Congratulations! You have signed up for an account.');
+	});
+
+	$('#info-chart').highcharts({
+                    chart: {
+                      type: 'column'
+                    },
+                    title: {
+                      text: 'Sub Regions Network & System Availability'
+                    },
+                    xAxis: {
+                      categories: <%= raw((@regions).map { |x| x["sub_region"] }) %>
+                    },
+                    yAxis: {
+                      title: {
+                      }
+                    },
+                    series: [{
+                      name: 'System Avg',
+                      color: '#99ff33 ',
+                      data: <%= (@regions).map { |x| x["system"] }.inspect%>
+                    }, {
+                      name: 'Network Avg',
+                      data: <%= (@regions).map { |x| x["network"] }%>
+                    },
+                    {
+                      type: 'spline',
+                      name: 'Target',
+                      data: <%= (@regions).map { |x| x["target"] }.inspect%>,
+                      marker: {
+                        lineWidth: 2,
+                        lineColor: Highcharts.getOptions().colors[3],
+                        fillColor: 'white'
+                      }
+                    }]
+                  });
+
 });
